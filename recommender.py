7ac235846b16d2fa36b5885d318ffec0790b86cf
AnnelieGroeneveld <46447106+AnnelieGroeneveld@users.noreply.github.com>
2019-05-13 15:37:02 +0200

    merge

--- recommender.py
+++ recommender.py
@@ -4,18 +4,14 @@ import random
 henk = 'Restaurants'
 piet = BUSINESSES['westlake']
 for x in piet:
-<<<<<<< HEAD
-    henk = x['categories']
-    for y in henk:
-        print(y)
-=======
+
     hallo = x['categories']
     test = hallo.split(',')
     for j in x['categories'].split(','):
         if j == 'Restaurants':
             print(x)
 
->>>>>>> ebdf7a71ccd7dc4461a3f2fdcf51f32411c009d2
+
 
 
 
