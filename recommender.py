fcd8b470efcc62324fabaff4db90511c986b3033
AnnelieGroeneveld <46447106+AnnelieGroeneveld@users.noreply.github.com>
2019-05-14 10:16:23 +0200

    merge

--- recommender.py
+++ recommender.py
@@ -9,26 +9,24 @@ for x in BUSINESSES['westlake']:
     for j in x['categories'].split(','):
         if j == 'Restaurants':
             lijst.append(x)
-<<<<<<< HEAD
-print(lijst)
-=======
+
     for j in lijst:
         if j['is_open'] == 0:
             lijst.remove(x)
 print(lijst)
 
 # filter ambiances uit lijst
->>>>>>> 6743fdfd527e44c28a7a8d580d6873f1bb5ce58c
-for i in lijst:
-    try:
-        test = i['attributes'].get("Ambience")
-        test = ast.literal_eval(test)
-        for k,v in test.items():
-            if v == True:
-                print(i['name'], k,v)
-    except:
-        continue
-
+def filter(categorie, subcategorie):
+    for i in lijst:
+        try:
+            test = i[categorie].get(subcategorie)
+            test = ast.literal_eval(test)
+            for k,v in test.items():
+                if v == True:
+                    print(i['name'], k,v)
+        except:
+            continue
+filter('attributes', "Ambience")
 
 
 
