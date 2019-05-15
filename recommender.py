8fcc4c8aa2bc19720470bba389f70f2df22b4b42
AnnelieGroeneveld <46447106+AnnelieGroeneveld@users.noreply.github.com>
2019-05-15 14:11:20 +0200

    merge

--- recommender.py
+++ recommender.py
@@ -93,14 +93,17 @@ def recommend(user_id=None, business_id=None, city=None, n=10):
     hallo = dict()
     for i in df_similarity_categories:
         hallo[i] = df_similarity_categories[business_id][i]
+    print(hallo)
     test = sorted(hallo, key=hallo.get, reverse=True)
     lijstje = []
     for i in test:
         for x in BUSINESSES['westlake']:
             if i == x['business_id']:
+                print(x)
                 lijstje.append(x)
     # print(lijstje)
-    return(lijstje[:-1])
+
+
 
 
     #if not city:
